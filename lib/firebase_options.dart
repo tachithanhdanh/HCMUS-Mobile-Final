6380923bb16a052c61514e7f3b4dc75f80e0d40c
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2025-01-15 13:04:47 +0700

    Add reconfig firebase

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,38 +2,36 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-/// Default [FirebaseOptions] for use with your Firebase apps.
-///
-/// Example:
-/// ```dart
-/// import 'firebase_options.dart';
-/// // ...
-/// await Firebase.initializeApp(
-///   options: DefaultFirebaseOptions.currentPlatform,
-/// );
-/// ```
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return FirebaseOptions(
-        apiKey: dotenv.env['FIREBASE_API_KEY_WEB'] ?? '',
-        appId: dotenv.env['FIREBASE_APP_ID_WEB'] ?? '',
-        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_WEB'] ?? '',
-        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
-        authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN_WEB'] ?? '',
-        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
-        measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID_WEB'] ?? '',
+        apiKey: dotenv.env['FIREBASE_API_KEY_WEB'] ?? 'MISSING_API_KEY_WEB',
+        appId: dotenv.env['FIREBASE_APP_ID_WEB'] ?? 'MISSING_APP_ID_WEB',
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_WEB'] ??
+            'MISSING_MESSAGING_SENDER_ID_WEB',
+        projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'MISSING_PROJECT_ID',
+        authDomain:
+            dotenv.env['FIREBASE_AUTH_DOMAIN_WEB'] ?? 'MISSING_AUTH_DOMAIN_WEB',
+        storageBucket:
+            dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'MISSING_STORAGE_BUCKET',
+        measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID_WEB'] ??
+            'MISSING_MEASUREMENT_ID_WEB',
       );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return FirebaseOptions(
-          apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID'] ?? '',
-          appId: dotenv.env['FIREBASE_APP_ID_ANDROID'] ?? '',
+          apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID'] ??
+              'MISSING_API_KEY_ANDROID',
+          appId:
+              dotenv.env['FIREBASE_APP_ID_ANDROID'] ?? 'MISSING_APP_ID_ANDROID',
           messagingSenderId:
-              dotenv.env['FIREBASE_MESSAGING_SENDER_ID_WEB'] ?? '',
-          projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
-          storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? '',
+              dotenv.env['FIREBASE_MESSAGING_SENDER_ID_ANDROID'] ??
+                  'MISSING_MESSAGING_SENDER_ID_ANDROID',
+          projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? 'MISSING_PROJECT_ID',
+          storageBucket:
+              dotenv.env['FIREBASE_STORAGE_BUCKET'] ?? 'MISSING_STORAGE_BUCKET',
         );
       default:
         throw UnsupportedError(
