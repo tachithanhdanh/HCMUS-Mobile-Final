5bc9c52fc9f8a2b924b0a9521bf2679eb0a4c77a
mvtkiet251 <mvtkiet251@gmail.com>
2025-01-18 18:58:09 +0700

    Remove debug at top-right corner

--- lib/app.dart
+++ lib/app.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Recipe App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
