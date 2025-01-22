695cfed77de7a95515073d5f9b1344460c5c5599
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2025-01-22 19:36:59 +0700

    Fix category overflow display bug

--- lib/views/categories_page.dart
+++ lib/views/categories_page.dart
@@ -11,103 +11,111 @@ class CategoriesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back, color: AppColors.redPinkMain),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                Text(
-                  "Categories",
-                  style: TextStyle(
-                    color: AppColors.redPinkMain,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                CategoriesIconActions()
-              ],
-            ),
-            const SizedBox(height: 16),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // Chia thành 2 cột
-                  crossAxisSpacing: 16.0, // Khoảng cách giữa các cột
-                  mainAxisSpacing: 16.0, // Khoảng cách giữa các hàng
-                  childAspectRatio: 1.15, // Tỉ lệ chiều cao/chiều rộng của mỗi ô
-                ),
-                itemCount: _categories.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => CategoriesDetailPage(
-                            selectedCategory: _categories[index],
-                          ),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.2),
-                            blurRadius: 8,
-                            spreadRadius: 2,
-                            offset: Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-                            child: Image.asset(
-                              '/images/${_categories[index].name}.jpg', // Tạo đường dẫn ảnh tự động
-                              fit: BoxFit.cover,
-                              height: 120,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Container(
-                                  // height: 120,
-                                  color: Colors.grey[200],
-                                  child: Icon(Icons.image_not_supported, color: Colors.grey),
-                                );
-                              },
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              _categories[index].name,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: AppColors.redPinkMain,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon:
+                          Icon(Icons.arrow_back, color: AppColors.redPinkMain),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    Text(
+                      "Categories",
+                      style: TextStyle(
+                        color: AppColors.redPinkMain,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  );
-                },
-              ),
+                    CategoriesIconActions()
+                  ],
+                ),
+                const SizedBox(height: 16),
+                GridView.builder(
+                  shrinkWrap: true, // Ensure it doesn't expand infinitely
+                  physics:
+                      NeverScrollableScrollPhysics(), // Prevent internal scrolling
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16.0,
+                    mainAxisSpacing: 16.0,
+                    childAspectRatio: 0.85, // Adjusted ratio
+                  ),
+                  itemCount: _categories.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => CategoriesDetailPage(
+                              selectedCategory: _categories[index],
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              blurRadius: 8,
+                              spreadRadius: 2,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(16)),
+                              child: Image.asset(
+                                '/images/${_categories[index].name}.jpg',
+                                fit: BoxFit.cover,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.15,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Container(
+                                    color: Colors.grey[200],
+                                    child: Icon(Icons.image_not_supported,
+                                        color: Colors.grey),
+                                  );
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                _categories[index].name,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: AppColors.redPinkMain,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
